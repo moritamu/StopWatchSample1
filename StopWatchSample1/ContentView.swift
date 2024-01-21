7149fde3cd445c1295084a7da2ee4635d85fdb23
morisum <moris@r.sannet.ne.jp>
2024-01-22 05:40:18 +0900

    Initial Commit

---
+++ StopWatchSample1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StopWatchSample1
+//
+//  Created by MsMacM on 2024/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StopWatchSample1/StopWatchSample1App.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchSample1App.swift
+//  StopWatchSample1
+//
+//  Created by MsMacM on 2024/01/22.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchSample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
